1f94331db50888200dde5264ff058a7ea478ee03
pa-matiscsakm <112852333+pa-MatiscsakM@users.noreply.github.com>
2022-10-03 09:24:35 +0200

    Add files via upload

---
+++ 9-10.ora.py
@@ -0,0 +1,10 @@
+# első feladat 9 DK
+idei_év = 2022 
+print(type(idei_év))
+print('az idei_év változó értéke:', idei_év, sep='--->')
+felhasználó_kora = input('Hány éves vagy')
+print(type(felhasználó_kora))
+print('Te most', felhasználó_kora, 'éves vagy')
+felhasználó_kora = int(felhasználó_kora)
+születési_év idei_év - felhasználó_kora
+print('Ekkor születtél: ', születési_év, '.', sep='')
