e6fa167a9cbf234e000cdbdd6212bfb7a5a413f9
pa-matiscsakm <112852333+pa-MatiscsakM@users.noreply.github.com>
2022-10-24 09:40:07 +0200

    Update 2022.10.24.py

--- 2022.10.24.py
+++ 2022.10.24.py
@@ -1,7 +1,46 @@
 a = "Hello, World!"
-print(a)
+
+print(a[1])
+
+print(a[4])
+
+for x in "banana":
+
+  print(x)
+  for x in "banana":
+ szam = 0
+    print(szam,x,sep="---",end="itt van a sor vége")
+    szam = szam +1
+
+a = "Hello, Woooorld!"
+
+print(len(a))
+
+txt = "The best things in life are free!"
+
+print("free" not in txt) 
+
+txt = "The best things in life are free!"
+
+if "expensive" not in txt:
+
+  print("No, 'expensive' is NOT present.")
+
+
+b = "Hello, World!"
+
+print(b[1:5])  
+
+     a = "Hello, World!"
+
 print(a.upper())
+
 
 a = "Hello, World!"
 
 print(a.lower())
+
+
+a = " Hello, World! "
+print(a)
+print(a.strip()) # returns "Hello, World!"
