b4cb473aeb537d44e9e2cbf61266d85d44bf47ea
pa-matiscsakm <112852333+pa-MatiscsakM@users.noreply.github.com>
2022-11-21 08:01:34 +0100

    Add files via upload

---
+++ 21-22.ora.py
@@ -0,0 +1,33 @@
+"""
+szam = 0
+while szam != 100:
+    szam =szam + 1
+    print(szam)
+print("A program vége!")
+# 1...100
+
+szam = 0
+while szam != 100:
+    print(szam)
+    szam =szam + 1
+print("A program vége!")
+#0...99
+
+szam = 1
+while True:
+    print(szam)
+    szam =szam + 1
+print("A program vége!")
+# a programa CTRL + C re tudjuk megállítani
+"""
+szam = 0
+while szam < 100:
+    szam = szam + 1
+    print(szam)
+print("A program vége!")
+
+
+
+
+
+
