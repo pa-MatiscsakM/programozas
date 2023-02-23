3efaf1646662bd9c37fea6a85c411222ee89fb63
pa-matiscsakm <112852333+pa-MatiscsakM@users.noreply.github.com>
2023-02-23 07:41:22 +0100

    Add files via upload

---
+++ MatiscsákMárk10D.py
@@ -0,0 +1,8 @@
+#MATISCSÁK MÁRK 10D
+szam=int(input("Adj meg egy számot!"))
+if szam > 0:
+    print("A megadott szám pozitív.")
+elif szam < 0:
+    print("A megadott szám negatív.")
+elif szam == 0:
+    print("A megadott szám nulla.")
