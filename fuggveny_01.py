394757a2ed5b9225c0bb29bdbffb9891af77fbb2
pa-matiscsakm <112852333+pa-MatiscsakM@users.noreply.github.com>
2023-03-23 08:30:31 +0100

    Add files via upload

---
+++ fuggveny_01.py
@@ -0,0 +1,17 @@
+"""x = ord("A")
+print(x)
+print(f"A nagy a betű kódja: {ord('A')}") # 65
+print(f"A kis a betű kódja: {ord('a')}") # 97
+
+x = chr(22)
+print(x)
+
+x = max(2, 17)
+print(x)
+
+x = min(4, 19)
+print(x)
+
+x = list(('apple', 'banana', 'cherry'))
+print(x)
+"""
---
+++ fuggveny_02.py
@@ -0,0 +1,104 @@
+"""
+txt = "Hello my FRIENDS"
+
+x = txt.lower()
+
+print(x)
+
+txt = "Hello my friends"
+
+x = txt.upper()
+
+print(x)
+
+txt = "hello, and welcome to my world."
+
+x = txt.capitalize()
+
+print (x)
+
+txt = "Hello, welcome to my world."
+
+x = txt.endswith(".")
+
+print(x)
+
+txt = "Hello, welcome to my world."
+
+x = txt.find("welcome")
+
+print(x)
+
+txt = "Company12"
+
+x = txt.isalnum()
+
+print(x)
+
+txt = "CompanyX"
+
+x = txt.isalpha()
+
+print(x)
+
+txt = "hello world!"
+
+x = txt.islower()
+
+print(x)
+
+myTuple = ("John", "Peter", "Vicky")
+
+x = "#".join(myTuple)
+
+print(x)
+
+txt = "I like bananas"
+
+x = txt.replace("bananas", "apples")
+
+print(x)
+
+txt = "Mi casa, su casa."
+
+x = txt.rfind("casa")
+
+print(x)
+
+txt = "     banana     "
+
+x = txt.rstrip()
+
+print("of all fruits", x, "is my favorite")
+
+txt = "Hello, welcome to my world."
+
+x = txt.startswith("Hello")
+
+print(x)
+
+txt = "     banana     "
+
+x = txt.strip()
+
+print("of all fruits", x, "is my favorite")
+
+txt = "Hello My Name Is PETER"
+
+x = txt.swapcase()
+
+print(x)
+
+txt = "Welcome to my world"
+
+x = txt.title()
+
+print(x)
+
+txt = "banana"
+
+x = txt.center(20)
+
+print(x)
+"""
+
